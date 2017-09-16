83a0a8d52d219fb7ba90b8c5df86774d11ba8380
TsHristov <tsvetanhristov@yahoo.com>
2017-09-16 23:24:59 +0300

    Remove unnecessary tuple check in evaluate()

--- Tasks/02/solution.py
+++ Tasks/02/solution.py
@@ -81,21 +81,17 @@ class Expression:
         lhs, _, rhs = expression_structure
         if type(lhs) is tuple:
             lhs = Expression(lhs)
-        elif type(rhs) is tuple:
+        if type(rhs) is tuple:
             rhs = Expression(rhs)
         return (lhs, _, rhs)
 
     def evaluate(self, **kwargs):
         if hasattr(self._lhs, 'evaluate'):
             lhs = self._lhs.evaluate(**kwargs)
-        elif type(self._lhs) is tuple:
-            lhs = Expression(self._lhs).evaluate(**kwargs)
         else:
             lhs = Constant(self._lhs).evaluate(**kwargs)
         if hasattr(self._rhs, 'evaluate'):
             rhs = self._rhs.evaluate(**kwargs)
-        elif type(self._rhs) is tuple:
-            rhs = Expression(self._rhs).evaluate(**kwargs)
         else:
             rhs = Constant(self._rhs).evaluate(**kwargs)
         return self._operator(lhs, rhs)
