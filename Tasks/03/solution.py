dee63e0c4b7a3bff9330a66eef9e71737c5939c8
TsHristov <tsvetanhristov@yahoo.com>
2017-09-19 16:07:10 +0300

    Cover main functionality as described in the tests

--- Tasks/03/solution.py
+++ Tasks/03/solution.py
@@ -1,6 +1,7 @@
 import uuid
 import datetime
 
+
 class UserDoesNotExistError(Exception):
     pass
 
@@ -13,29 +14,47 @@ class UsersNotConnectedError(Exception):
     pass
 
 
+class Post:
+    def __init__(self, uuid, content):
+        self._author = uuid
+        self._published_at = datetime.datetime.now()
+        self._content = content
+
+    @property
+    def author(self):
+        return self._author
+
+    @property
+    def content(self):
+        return self._content
+
+    @property
+    def published_at(self):
+        return self._published_at
+
+    
 class User:
     MAX_POSTS = 50
 
     def __init__(self, full_name):
         self._full_name = full_name
         self._uuid = uuid.uuid4()
+        self._posts = []
 
     @property
     def uuid(self):
         return self._uuid
 
     def add_post(self, post_content):
-        pass
+        """Create a new Post for the User."""
+        self._posts.append(Post(self.uuid, post_content))
 
     def get_post(self):
-        pass
+        """Return generator over the User`s Posts."""
+        for post in self._posts:
+            yield post
 
-
-class Post:
-    def __init__(self):
-        pass
-
-
+            
 class SocialGraph:
     def __init__(self):
         self._graph = {}
@@ -43,6 +62,8 @@ class SocialGraph:
     def _find_by_uuid(self, uuid):
         """Return User with uuid=uuid"""
         user = [_ for _ in self._graph if _.uuid == uuid]
+        if user:
+            return user[0]
 
     def add_user(self, user):
         """Add an user in the graph."""
@@ -76,19 +97,18 @@ class SocialGraph:
         return followee in self._graph[follower]
 
     def followers(self, user_uuid):
-        """Return set of all users following a given user with uuid=user_uuid"""
+        """Return set of all users following user_uuid"""
         pass
 
     def following(self, user_uuid):
-        """Return set of all uuid`s followed by user with uuid=user_uuid"""
+        """Return set of all uuid`s followed by user with uuid=user_uuid."""
         pass
 
     def friends(self, user_uuid):
-        """Return set of all users that are friends with user with uuid=user_uuid"""
-        user = self._find_by_uuid(user_uuid)
-        friends = {_ for _ in self._graph if self.is_following(_, user) and self.is_following(user, _)}
-        return friends
-        # friends if both users follow each other
+        """Return set of all users that are friends with user_uuid."""
+        return {_.uuid for _ in self._graph if
+                self.is_following(_.uuid, user_uuid) and
+                self.is_following(user_uuid, _.uuid)}
 
     def max_distance(self, user_uuid):
         pass
